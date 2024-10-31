bdde5b1b67faa5761b4252e2e629b656e704a480
Eric Oriez <ericoriez@live.fr>
2024-10-31 10:01:56 +0100

    first commit

---
+++ src/main/java/com/ebm/findmycar/FindMyCarApplication.java
@@ -0,0 +1,13 @@
+package com.ebm.findmycar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FindMyCarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FindMyCarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ebm/findmycar/FindMyCarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebm.findmycar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindMyCarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
